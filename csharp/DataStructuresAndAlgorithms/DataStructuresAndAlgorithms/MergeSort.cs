0ed5f41c6c44eb2f202f44612d86eef624579cf2
Burhan SAVCI <burhansavci@gmail.com>
2020-03-08 13:51:12 +0300

    Add MergeSort for CSharp

---
+++ csharp/DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/MergeSort.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class MergeSort
+    {
+        /// <summary>
+        /// Sort the given array by mergesort.
+        /// </summary>
+        /// <param name="arr">The array to be sorted.</param>
+        public static void Sort(int[] arr)
+        {
+            Sort(arr, 0, arr.Length - 1);
+        }
+        /// <summary>
+        /// Recursively calculate the middle pivot and sort the given array, merging each array as it recurses.
+        /// </summary>
+        /// <param name="arr">The array to be sorted.</param>
+        /// <param name="lowHalfStart">The start index of lower half of the given array.</param>
+        /// <param name="highHalfEnd">The end index of higher half of the given array.</param>
+        static void Sort(int[] arr, int lowHalfStart, int highHalfEnd)
+        {
+            if (lowHalfStart < highHalfEnd)
+            {
+                int pivot = (lowHalfStart + highHalfEnd) / 2;
+                Sort(arr, lowHalfStart, pivot);
+                Sort(arr, pivot + 1, highHalfEnd);
+                Merge(arr, lowHalfStart, pivot, highHalfEnd);
+            }
+        }
+        /// <summary>
+        /// Takes in an array that has two sorted subarrays, from [lowHalfStart..pivot] and [pivot+1..highHalfEnd], and merges the array.
+        /// </summary>
+        /// <param name="arr">The array to be merged.</param>
+        /// <param name="lowHalfStart">The start index of lower half of the given array.</param>
+        /// <param name="pivot">The middle index of the given array.</param>
+        /// <param name="highHalfEnd">The end index of higher half of the given array.</param>
+        static void Merge(int[] arr, int lowHalfStart, int pivot, int highHalfEnd)
+        {
+            int[] lowHalf = new int[pivot - lowHalfStart + 1];
+            int[] highHalf = new int[highHalfEnd - pivot];
+
+            int arrPointer = lowHalfStart;
+            //Copy the given array into lowHalf and highHalf. 
+            for (int i = 0; arrPointer <= pivot; i++, arrPointer++)
+            {
+                lowHalf[i] = arr[arrPointer];
+            }
+            for (int i = 0; arrPointer <= highHalfEnd; i++, arrPointer++)
+            {
+                highHalf[i] = arr[arrPointer];
+            }
+
+            arrPointer = lowHalfStart;
+            int lowHalfPointer = 0;
+            int highHalfPointer = 0;
+            // Repeatedly compare the lowest untaken element in
+            // lowHalf with the lowest untaken element in highHalf
+            // and copy the lower of the two back into arr.
+            while (lowHalfPointer < lowHalf.Length && highHalfPointer < highHalf.Length)
+            {
+                if (lowHalf[lowHalfPointer] < highHalf[highHalfPointer])
+                {
+                    arr[arrPointer] = lowHalf[lowHalfPointer];
+                    lowHalfPointer++;
+                }
+                else
+                {
+                    arr[arrPointer] = highHalf[highHalfPointer];
+                    highHalfPointer++;
+                }
+                arrPointer++;
+            }
+
+            // Once one of lowHalf and highHalf has been fully copied
+            // back into arr, copy the remaining elements from the
+            // other temporary array back into the arr.
+            while (lowHalfPointer < lowHalf.Length)
+            {
+                arr[arrPointer] = lowHalf[lowHalfPointer];
+                lowHalfPointer++;
+                arrPointer++;
+            }
+            while (highHalfPointer < highHalf.Length)
+            {
+                arr[arrPointer] = highHalf[highHalfPointer];
+                highHalfPointer++;
+                arrPointer++;
+            }
+        }
+    }
+}
--- csharp/DataStructuresAndAlgorithms/DataStructuresAndAlgorithmsTest/TestEnvironment.cs
+++ csharp/DataStructuresAndAlgorithms/DataStructuresAndAlgorithmsTest/TestEnvironment.cs
@@ -1,7 +1,4 @@
 ﻿using DataStructuresAndAlgorithms;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace DataStructuresAndAlgorithmsTest
@@ -56,6 +53,19 @@ namespace DataStructuresAndAlgorithmsTest
             InsertionSort.Sort(arr1);
             InsertionSort.Sort(arr2);
             InsertionSort.Sort(arr3);
+            Assert.Equal(arr1, new int[] { -23, -10, 1, 2, 3, 4, 5 });
+            Assert.Equal(arr2, new int[] { -10, -10, 0, 2, 2, 3, 3, 4, 9, 55 });
+            Assert.Equal(arr3, new int[] { -78, -10, 0, 1, 2, 3, 4, 5, 99, 100 });
+        }
+        [Fact]
+        public void MergeSortTest()
+        {
+            int[] arr1 = new int[] { 3, 4, 5, 1, 2, -23, -10 };
+            int[] arr2 = new int[] { 9, 2, 3, 2, 4, 3, -10, 55, -10, 0 };
+            int[] arr3 = new int[] { 100, 99, 5, 4, 3, 2, 1, 0, -10, -78 };
+            MergeSort.Sort(arr1);
+            MergeSort.Sort(arr2);
+            MergeSort.Sort(arr3);
             Assert.Equal(arr1, new int[] { -23, -10, 1, 2, 3, 4, 5 });
             Assert.Equal(arr2, new int[] { -10, -10, 0, 2, 2, 3, 3, 4, 9, 55 });
             Assert.Equal(arr3, new int[] { -78, -10, 0, 1, 2, 3, 4, 5, 99, 100 });

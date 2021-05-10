aca327700b21bebbc6bb4613a5e290fa0a5ec707
Burhan SAVCI <burhansavci@gmail.com>
2021-05-10 16:59:11 +0300

    Add DoublyLinkedList

---
+++ csharp/src/DataStructures/LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures.LinkedList
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        private int size;
+        private Node<T> head;
+        private Node<T> tail;
+
+        // Internal node class to represent data
+        private class Node<TY>
+        {
+            public TY Data;
+            public Node<TY> Prev;
+            public Node<TY> Next;
+
+            public Node(TY data, Node<TY> prev, Node<TY> next)
+            {
+                Data = data;
+                Prev = prev;
+                Next = next;
+            }
+        }
+
+        // Empty this linked list, O(n)
+        public void Clear()
+        {
+            var currentNode = head;
+            while (currentNode != null)
+            {
+                var next = currentNode.Next;
+                currentNode.Prev = currentNode.Next = null;
+                currentNode.Data = default;
+                currentNode = next;
+            }
+            head = tail = null;
+            size = 0;
+        }
+
+        // Return the size of this linked list
+        public int Count => size;
+
+        // Is this linked list empty?
+        public bool IsEmpty() => Count == 0;
+
+        // Add an element to the tail of the linked list, O(1)
+        public void Add(T data)
+        {
+            AddLast(data);
+        }
+
+        // Add a node to the tail of the linked list, O(1)
+        public void AddLast(T data)
+        {
+            if (IsEmpty())
+                head = tail = new Node<T>(data, null, null);
+            else
+            {
+                tail.Next = new Node<T>(data, tail, null);
+                tail = tail.Next;
+            }
+            size++;
+        }
+
+        // Add an element to the beginning of this linked list, O(1)
+        public void AddFirst(T data)
+        {
+            if (IsEmpty())
+                head = tail = new Node<T>(data, null, null);
+            else
+            {
+                head.Prev = new Node<T>(data, null, head);
+                head = head.Prev;
+            }
+            size++;
+        }
+
+        // Add an element at a specified index
+        public void AddAt(int index, T data)
+        {
+            if (index < 0 || index > Count)
+                throw new IndexOutOfRangeException();
+
+            if (index == 0)
+                AddFirst(data);
+            else if (index == Count)
+                AddLast(data);
+            else
+            {
+                var currentNode = head;
+                for (var i = 0; i < index - 1; i++)
+                    currentNode = currentNode.Next;
+
+                var newNode = new Node<T>(data, currentNode, currentNode.Next);
+                currentNode.Next.Prev = newNode;
+                currentNode.Next = newNode;
+                size++;
+            }
+        }
+
+        // Check the value of the first node if it exists, O(1)
+        public T PeekFirst()
+        {
+            if (IsEmpty()) throw new IndexOutOfRangeException("List is empty.");
+            return head.Data;
+        }
+
+        // Check the value of the last node if it exists, O(1)
+        public T PeekLast()
+        {
+            if (IsEmpty()) throw new IndexOutOfRangeException("List is empty.");
+            return tail.Data;
+        }
+
+        // Remove the first value at the head of the linked list, O(1)
+        public T RemoveFirst()
+        {
+            if (IsEmpty()) throw new IndexOutOfRangeException("List is empty.");
+
+            // Extract the data at the head and move
+            // the head pointer forwards one node
+            var data = head.Data;
+            head = head.Next;
+            size--;
+
+            if (IsEmpty())
+                tail = null; // If the list is empty set the tail to null
+            else
+                head.Prev = null; // Do a memory cleanup of the previous node
+
+            // Return the data that was at the first node we just removed
+            return data;
+        }
+
+        // Remove the last value at the tail of the linked list, O(1)
+        public T RemoveLast()
+        {
+            if (IsEmpty()) throw new IndexOutOfRangeException("List is empty.");
+
+            // Extract the data at the tail and move
+            // the tail pointer backwards one node
+            var data = tail.Data;
+            tail = tail.Prev;
+            size--;
+
+            if (IsEmpty())
+                head = null; // If the list is empty set the head to null
+            else
+                tail.Next = null; // Do a memory clean of the node that was just removed
+
+            // Return the data that was at the last node we just removed
+            return data;
+        }
+
+        // Remove an arbitrary node from the linked list, O(1)
+        private T Remove(Node<T> node)
+        {
+            // If the node to remove is somewhere either at the
+            // head or the tail handle those independently
+            if (node.Prev == null) return RemoveFirst();
+            if (node.Next == null) return RemoveLast();
+
+            // Make the pointers of adjacent nodes skip over 'node'
+            node.Next.Prev = node.Prev;
+            node.Prev.Next = node.Next;
+
+            // Temporarily store the data we want to return
+            var data = node.Data;
+
+            // Memory cleanup
+            node.Data = default;
+            node = node.Prev = node.Next = null;
+
+            size--;
+
+            // Return the data in the node we just removed
+            return data;
+        }
+
+        // Remove a node at a particular index, O(n)
+        public T RemoveAt(int index)
+        {
+            // Handle index out of bound errors
+            if (IsEmpty() || index < 0 || index >= Count)
+                throw new IndexOutOfRangeException();
+
+            int i;
+            Node<T> currentNode;
+
+            // Search from the front of the list
+            if (index < size / 2)
+                for (i = 0, currentNode = head; i != index; i++)
+                    currentNode = currentNode.Next;
+            // Search from the back of the list
+            else
+                for (i = size - 1, currentNode = tail; i != index; i--)
+                    currentNode = currentNode.Prev;
+
+            return Remove(currentNode);
+        }
+
+        // Remove a particular value in the linked list, O(n)
+        public bool Remove(T data)
+        {
+            for (var currentNode = head; currentNode != null; currentNode = currentNode.Next)
+                if (Equals(currentNode.Data, data))
+                {
+                    Remove(currentNode);
+                    return true;
+                }
+
+            return false;
+        }
+
+        // Find the index of a particular value in the linked list, O(n)
+        public int IndexOf(T data)
+        {
+            var index = 0;
+            for (var currentNode = head; currentNode != null; currentNode = currentNode.Next, index++)
+                if (Equals(currentNode.Data, data))
+                    return index;
+
+            return -1;
+        }
+
+        // Check is a value is contained within the linked list
+        public bool Contains(T data) => IndexOf(data) != -1;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = head;
+            while (node != null)
+            {
+                yield return node.Data;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ csharp/test/DataStructuresTest/LinkedListTest/DoublyLinkedListTest.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataStructures.LinkedList;
+using Xunit;
+
+namespace DataStructuresTest.LinkedListTest
+{
+    public class DoublyLinkedListTest
+    {
+        private readonly DoublyLinkedList<int?> list;
+        private static readonly int Loops = 10000;
+        private static readonly int TestSz = 40;
+        private static readonly int NumNulls = TestSz / 5;
+
+        public DoublyLinkedListTest() => list = new DoublyLinkedList<int?>();
+
+        [Fact]
+        public void TestEmptyList()
+        {
+            Assert.Empty(list);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void TestRemoveFirstOfEmpty() => Assert.Throws<IndexOutOfRangeException>(() => list.RemoveFirst());
+
+        [Fact]
+        public void TestRemoveLastOfEmpty() => Assert.Throws<IndexOutOfRangeException>(() => list.RemoveLast());
+
+        [Fact]
+        public void TestPeekFirstOfEmpty() => Assert.Throws<IndexOutOfRangeException>(() => list.PeekFirst());
+
+        [Fact]
+        public void TestPeekLastOfEmpty() => Assert.Throws<IndexOutOfRangeException>(() => list.PeekLast());
+
+
+        [Fact]
+        public void TestAddFirst()
+        {
+            list.AddFirst(3);
+            Assert.Equal(1, list.Count);
+            list.AddFirst(5);
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void TestAddLast()
+        {
+            list.AddLast(3);
+            Assert.Equal(1, list.Count);
+            list.AddLast(5);
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void TestAddAt()
+        {
+            list.AddAt(0, 1);
+            Assert.Equal(1, list.Count);
+
+            list.AddAt(1, 2);
+            Assert.Equal(2, list.Count);
+
+            list.AddAt(1, 3);
+            Assert.Equal(3, list.Count);
+
+            list.AddAt(2, 4);
+            Assert.Equal(4, list.Count);
+
+            list.AddAt(1, 8);
+            Assert.Equal(5, list.Count);
+        }
+
+
+        [Fact]
+        public void TestRemoveFirst()
+        {
+            list.AddFirst(3);
+            Assert.Equal(3, list.RemoveFirst());
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void TestRemoveLast()
+        {
+            list.AddLast(4);
+            Assert.Equal(4, list.RemoveLast());
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void TestPeekFirst()
+        {
+            list.AddFirst(4);
+            Assert.Equal(4, list.PeekFirst());
+            Assert.Equal(1, list.Count);
+        }
+
+        [Fact]
+        public void TestPeekLast()
+        {
+            list.AddLast(4);
+            Assert.Equal(4, list.PeekLast());
+            Assert.Equal(1, list.Count);
+        }
+
+        [Fact]
+        public void TestPeeking()
+        {
+            // 5
+            list.AddFirst(5);
+            Assert.Equal(5, list.PeekFirst());
+            Assert.Equal(5, list.PeekLast());
+
+            // 6 - 5
+            list.AddFirst(6);
+            Assert.Equal(6, list.PeekFirst());
+            Assert.Equal(5, list.PeekLast());
+
+            // 7 - 6 - 5
+            list.AddFirst(7);
+            Assert.Equal(7, list.PeekFirst());
+            Assert.Equal(5, list.PeekLast());
+
+
+            // 7 - 6 - 5 - 8
+            list.AddLast(8);
+            Assert.Equal(7, list.PeekFirst());
+            Assert.Equal(8, list.PeekLast());
+
+            // 7 - 6 - 5
+            list.RemoveLast();
+            Assert.Equal(7, list.PeekFirst());
+            Assert.Equal(5, list.PeekLast());
+
+            // 7 - 6
+            list.RemoveLast();
+            Assert.Equal(7, list.PeekFirst());
+            Assert.Equal(6, list.PeekLast());
+
+            // 6
+            list.RemoveFirst();
+            Assert.Equal(6, list.PeekFirst());
+            Assert.Equal(6, list.PeekLast());
+        }
+
+        [Fact]
+        public void TestRemoving()
+        {
+            var strings = new DoublyLinkedList<string>
+            {
+                    "a",
+                    "b",
+                    "c",
+                    "d",
+                    "e",
+                    "f"
+            };
+            strings.Remove("b");
+            strings.Remove("a");
+            strings.Remove("d");
+            strings.Remove("e");
+            strings.Remove("c");
+            strings.Remove("f");
+            Assert.Equal(0, strings.Count);
+        }
+
+        [Fact]
+        public void TestRemoveAt()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.RemoveAt(0);
+            list.RemoveAt(2);
+            Assert.Equal(2, list.PeekFirst());
+            Assert.Equal(3, list.PeekLast());
+            list.RemoveAt(1);
+            list.RemoveAt(0);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void TestClear()
+        {
+            list.Add(22);
+            list.Add(33);
+            list.Add(44);
+            Assert.Equal(3, list.Count);
+            list.Clear();
+            Assert.Equal(0, list.Count);
+
+            list.Add(22);
+            list.Add(33);
+            list.Add(44);
+            Assert.Equal(3, list.Count);
+            list.Clear();
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void TestRandomizedRemoving()
+        {
+            var systemLinkedList = new LinkedList<int?>();
+            for (var loops = 0; loops < Loops; loops++)
+            {
+                list.Clear();
+                systemLinkedList.Clear();
+
+                var randNums = GenRandList(TestSz);
+                foreach (var value in randNums)
+                {
+                    systemLinkedList.AddLast(value);
+                    list.Add(value);
+                }
+                Shuffle(randNums);
+
+                foreach (var rmVal in randNums)
+                {
+                    Assert.Equal(systemLinkedList.Remove(rmVal), list.Remove(rmVal));
+                    Assert.Equal(systemLinkedList.Count, list.Count);
+
+                    using var iter1 = systemLinkedList.GetEnumerator();
+                    using var iter2 = list.GetEnumerator();
+                    while (iter1.MoveNext() && iter2.MoveNext())
+                        Assert.Equal(iter1.Current, iter2.Current);
+                }
+
+                list.Clear();
+                systemLinkedList.Clear();
+
+                foreach (var value in randNums)
+                {
+                    systemLinkedList.AddLast(value);
+                    list.Add(value);
+                }
+
+                var rand = new Random();
+                // Try removing elements whether or not they exist
+                for (int i = 0; i < randNums.Count; i++)
+                {
+                    var rmVal = rand.Next(0, list.Count);
+                    Assert.Equal(systemLinkedList.Remove(rmVal), list.Remove(rmVal));
+                    Assert.Equal(systemLinkedList.Count, list.Count);
+
+                    using var iter1 = systemLinkedList.GetEnumerator();
+                    using var iter2 = list.GetEnumerator();
+                    while (iter1.MoveNext() && iter2.MoveNext())
+                        Assert.Equal(iter1.Current, iter2.Current);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestRandomizedRemoveAt()
+        {
+            var systemLinkedList = new LinkedList<int?>();
+
+            for (var loops = 0; loops < Loops; loops++)
+            {
+                list.Clear();
+                systemLinkedList.Clear();
+
+                var randNums = GenRandList(TestSz);
+
+                foreach (var value in randNums)
+                {
+                    systemLinkedList.AddLast(value);
+                    list.Add(value);
+                }
+
+                var rand = new Random();
+                for (var i = 0; i < randNums.Count; i++)
+                {
+                    var rmIndex = rand.Next(0, list.Count);
+
+                    var num1 = RemoveAt(systemLinkedList, rmIndex).Value;
+                    var num2 = list.RemoveAt(rmIndex);
+
+                    Assert.Equal(num1, num2);
+                    Assert.Equal(systemLinkedList.Count, list.Count);
+
+                    using var iter1 = systemLinkedList.GetEnumerator();
+                    using var iter2 = list.GetEnumerator();
+                    while (iter1.MoveNext() && iter2.MoveNext())
+                        Assert.Equal(iter1.Current, iter2.Current);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestRandomizedIndexOf()
+        {
+            var systemLinkedList = new LinkedList<int?>();
+
+            for (var loops = 0; loops < Loops; loops++)
+            {
+                systemLinkedList.Clear();
+                list.Clear();
+
+                var randNums = GenUniqueRandList(TestSz);
+
+                foreach (var value in randNums)
+                {
+                    systemLinkedList.AddLast(value);
+                    list.Add(value);
+                }
+
+                Shuffle(randNums);
+
+                foreach (var elem in randNums)
+                {
+                    int index1 = systemLinkedList.Select((item, inx) => new {item, inx})
+                                                 .First(x => x.item == elem).inx;
+                    var index2 = list.IndexOf(elem);
+
+                    Assert.Equal(index1, index2);
+                    Assert.Equal(systemLinkedList.Count, list.Count);
+
+                    using var iter1 = systemLinkedList.GetEnumerator();
+                    using var iter2 = list.GetEnumerator();
+                    while (iter1.MoveNext() && iter2.MoveNext())
+                        Assert.Equal(iter1.Current, iter2.Current);
+                }
+            }
+        }
+
+        // Generate a list of random numbers
+        private static List<int?> GenRandList(int sz)
+        {
+            var rand = new Random();
+
+            var list = new List<int?>(sz);
+            for (var i = 0; i < sz; i++) list.Add(rand.Next());
+            for (var i = 0; i < NumNulls; i++) list.Add(null);
+            Shuffle(list);
+            return list;
+        }
+
+        // Generate a list of unique random numbers
+        private static List<int?> GenUniqueRandList(int sz)
+        {
+            var list = new List<int?>(sz);
+            for (var i = 0; i < sz; i++) list.Add(i);
+            for (var i = 0; i < NumNulls; i++) list.Add(null);
+            Shuffle(list);
+            return list;
+        }
+
+        private static void Shuffle<T>(IList<T> list)
+        {
+            var rand = new Random();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rand.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        private static LinkedListNode<T> RemoveAt<T>(LinkedList<T> list, int index)
+        {
+            var currentNode = list.First;
+            for (int i = 0; i <= index && currentNode != null; i++)
+            {
+                if (i != index)
+                {
+                    currentNode = currentNode.Next;
+                    continue;
+                }
+
+                list.Remove(currentNode);
+                return currentNode;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
